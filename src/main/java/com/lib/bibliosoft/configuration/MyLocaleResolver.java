6c8c852df69ed1025fa92279d08da6a4f646c545
JevenM <2446026601@qq.com>
2018-10-06 17:32:10 +0800

    locale configuration

---
+++ src/main/java/com/lib/bibliosoft/configuration/MyLocaleResolver.java
@@ -0,0 +1,12 @@
+package com.lib.bibliosoft.configuration;
+
+/**
+ * @Author: 毛文杰
+ * @Description: web internationalization, we can carry regional information on the link
+ * @Date: Created in 5:28 PM. 10/6/2018
+ * @Modify By:
+ */
+public class MyLocaleResolver {
+
+
+}
